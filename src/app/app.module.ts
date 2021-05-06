518fc427c95e01631935b2a6cca8a834c66a3a44
hari.mj <hari.mj@incedoinc.com>
2021-05-06 19:54:36 +0530

    add ui for product list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckoutFormComponent } from './checkout-form/checkout-form.component';
+import { OrderConfirmationComponent } from './order-confirmation/order-confirmation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductDetailsComponent,
+    ShoppingCartComponent,
+    CheckoutFormComponent,
+    OrderConfirmationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {ProductsService} from '../services/products.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+  product_id: string = '1';
+  product: any = {};
+
+  constructor(private actRoute: ActivatedRoute, private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.product = this.productsService.getProducts().filter(product => product.id === parseInt(this.actRoute.snapshot.params.id))[0];
+  }
+
+}
